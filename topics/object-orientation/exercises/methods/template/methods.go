fe146530766fae7a9d47431790b9044717a71c44
Lunamor <hongyi.meng@jigjog.org>
2021-07-05 21:09:10 -0500

    LC-22 #comment done methods

--- topics/object-orientation/exercises/methods/template/methods.go
+++ topics/object-orientation/exercises/methods/template/methods.go
@@ -7,18 +7,32 @@
 package main
 
 // Add your imports here.
+import "fmt"
 
 // Declare a struct type `player` to maintain information about a player.
+type player struct {
+	name string
+	numMatchesPlayed int
+	numMatchesWon int
+}
 
 // Declare a method that calculates the win ratio for the player.  Note that
 // you'll likely need to convert one or more values to floats, which can be
 // done like: float32(intValue)
-func ( /* receiver */ ) funcName() /* return type */ {
+func (p player) winRatio() float32 {
+	return float32(p.numMatchesWon) / float32(p.numMatchesPlayed)
 }
 
 func main() {
 	// Create a new player, and output their win ratio.
+	p1 := player{"Mike", 100, 32}
+	winRatio := p1.winRatio()
+	fmt.Printf("%s: %f\n", p1.name, winRatio)
 
 	// If you're feeling adventurous, try creating a slice of multiple players
 	// and iterating over the slice, displaying the player name and win ratio.
+	players := []player{{"Melissa", 150, 93}, {"Edward", 84, 32}}
+	for _, player := range players {
+		fmt.Printf("%s: %f\n", player.name, player.winRatio())
+	}
 }
