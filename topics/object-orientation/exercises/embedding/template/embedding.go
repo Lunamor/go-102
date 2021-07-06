2e2839c65a6d62ce470bb33daab554d73de78814
Lunamor <hongyi.meng@jigjog.org>
2021-07-06 15:28:04 -0500

    LC-22 #comment done embedding

--- topics/object-orientation/exercises/embedding/template/embedding.go
+++ topics/object-orientation/exercises/embedding/template/embedding.go
@@ -7,21 +7,42 @@
 package main
 
 // Add your imports here.
+import (
+	"fmt"
+)
 
 // Define a `notifier` interface.
+type notifier interface {
+	notify()
+}
 
 // Create a `user` type, with fields for name and email address.  Ensure your
 // type satisfies the Notifier interface.
+type user struct {
+	name string
+	email string
+}
+
+func (u user) notify() {
+	fmt.Printf("Sending email to %s at %s", u.name, u.email)
+}
 
 // Create an `admin` type which embeds a user, and has a security level.
+type admin struct {
+	user
+	securityLevel int
+}
 
 // Write a function that accepts a value of your interface and calls the method
 // associated with that interface.
-func funcName( /* args */ ) {
+func notify(notifier notifier) {
+	notifier.notify()
 }
 
 func main() {
 	// Create an admin user.
+	admin := admin{user{"Mike", "Mike@gmail.com"}, 5}
 
 	// Send the admin a notification via the function you created.
+	notify(admin)
 }
