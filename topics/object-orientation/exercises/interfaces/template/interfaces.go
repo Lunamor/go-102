32178f5c7d9bbb90df89bf20441790750ab92e24
Lunamor <hongyi.meng@jigjog.org>
2021-07-05 22:03:14 -0500

    LC-22 #comment done interfaces

--- topics/object-orientation/exercises/interfaces/template/interfaces.go
+++ topics/object-orientation/exercises/interfaces/template/interfaces.go
@@ -7,22 +7,68 @@
 package main
 
 // Add your imports here.
+import (
+	"math"
+	"fmt"
+)
 
 // Define an interface with a method area().  Make sure you use a meaningful
 // name and a sensible return type.
+
+type areaer interface {
+	area() float32
+}
 
 // Create square, rectangle and circle types, and ensure they satisfy your
 // interface (you'll need to use the `Pi` constant from the `math` package for
 // your circle).
 
+type square struct {
+	side float32
+}
+
+func (s square) area() float32 {
+	return s.side * s.side
+}
+
+type rectangle struct {
+	width float32
+	height float32
+}
+
+func (r rectangle) area() float32 {
+	return r.width * r.height
+}
+
+type circle struct {
+	radius float32
+}
+
+func (c circle) area() float32 {
+	return math.Pi * c.radius * c.radius
+}
+
 // Write a function that accepts a value of your interface and outputs the
 // area.
-func funcName( /* args */ ) {}
+
+func outputArea(shape areaer) {
+	fmt.Printf("%T with area %f\n", shape, shape.area())
+}
 
 func main() {
 	// Create a slice of your interface type, and populate it with a number of
 	// different shapes.
 
+	shapes := []areaer{
+		square{4.3},
+		rectangle{2, 3.4},
+		circle{2.2},
+	}
+
 	// Loop through your shapes and use your function to output the area of
 	// each one.
+
+	for _, shape := range shapes {
+		outputArea(shape)
+	}
 }
