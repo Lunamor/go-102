23044a196abf56fb91f85d3a37ed3ca79cefe0f1
Lunamor <hongyi.meng@jigjog.org>
2021-07-06 16:46:10 -0500

    LC-24 #comment done channels

--- topics/concurrency/exercises/channels/template/channels.go
+++ topics/concurrency/exercises/channels/template/channels.go
@@ -6,39 +6,59 @@
 package main
 
 // Add your imports here.
+import (
+	"fmt"
+	"sync"
+
+)
 
 // Create a new waitgroup.
+var wg sync.WaitGroup
 
 func main() {
 	// Add something to the waitgroup so main() can wait for the race to
 	// finish.
+	wg.Add(1)
 
 	// Create an unbuffered channel to act as the track.  In terms of the
 	// channel type, think about how you're going to count the runners.
+	track := make(chan int)
 
 	// Put the first runner at his starting position by launching the
 	// `runner()` function as a goroutine.
+	go runner(track)
 
 	// Give the runner the baton by sending it on the channel.
+	track <- 1
 
 	// Wait until the race is over.
+	wg.Wait()
+
+	fmt.Println("The race is over")
 }
 
 // Create the function representing the runner.  They'll need to be given the
 // track channel so we can pass the baton between runners.
-func funcName( /* arguments */ ) {
+func runner(track chan int) {
 	// Set the runner ready to receive the baton by receiving on the channel.
+	fmt.Println("New runner ready to receive the baton")
+	number := <- track
 
+	fmt.Println("Runner is running leg", number)
 	// If this runner is the fourth runner then the race is over.
-	{
+	if number == 4 {
 		// Signify the race is over by decrementing the waitgroup.
-
+		wg.Done()
 		// End this function here so we don't line up another runner.
+		return
 	}
 
 	// Put the next runner at their starting position.
+	go runner(track)
 
 	// Incremember the count of the baton.
+	number++
 
 	// Pass the baton to the next runner by sending it on the channel.
+	track <- number
 }
